a75146d60816cc12830abed62baf915400431fb5
jukka211 <158295066+jukka211@users.noreply.github.com>
2024-04-15 19:04:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeText(id) {
-  document.getElementById("changeable-text").innerHTML = id + ".fps";
+  document.getElementById("changeable-text").innerHTML = id + "FPS";
 }
 
 function restoreText() {
-  document.getElementById("changeable-text").innerHTML = "23.fps";
+  document.getElementById("changeable-text").innerHTML = "23FPS";
 }
 
 function triggerTextChange(id) {
@@ -26,28 +26,77 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function() {
   $('.column').click(function() {
      $(this).css('mix-blend-mode', 'normal');
+     
+     
+     
   });
 });
 
 
 $(document).ready(function() {
-   $('.column').hover(
-     function() {
-       // Check if the screen height is higher than 1500px
-       if ($(window).height() > 1500) {
-         $(this).css('flex', '18.8'); 
-       } else if ($(window).height() > 1000) {
-         $(this).css('flex', '14.8');
-       } 
-       else {
-         $(this).css('flex', '11'); $(this).css('mix-blend-mode', 'difference'); 
-       }
-     }, 
-     function() {
-       $(this).css('flex', '1');$(this).css('transform', 'translateX(0%)'); $(this).css('mix-blend-mode', 'exclusion'); 
-     }
-   );
+  // Check if the device is a desktop
+  if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+    // Code to execute only on desktops
+    $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').click(function() {
+      var flexValue;
+      if ($(window).height() > 1500) {
+        flexValue = '18.8';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else if ($(window).height() > 1000) {
+        flexValue = '20';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else {
+        flexValue = '20';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      }
+      $(this).css('flex', flexValue);
+    });
+  }
 });
+
+
+
+//Sonst einfach löschen
+$(document).ready(function() {
+  // Check if the device is a desktop
+  if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+    // Code to execute only on desktops
+    $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').hover(function() {
+      var flexValue;
+      if ($(window).height() > 1500) {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else if ($(window).height() > 1000) {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      } else {
+        flexValue = '3';
+        $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
+      }
+      $(this).css('flex', flexValue);
+    });
+  }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Select all images within the .column class
+  var images = document.querySelectorAll('.column img');
+
+  // Function to change visibility of images with fadeIn animation after a delay
+  function showImages() {
+    images.forEach(function(img, index) {
+      // Add fadeIn animation to each image
+      img.style.visibility = 'visible';
+     
+    });
+  }
+
+  // Set a timeout to call the showImages function after a 500ms delay
+  setTimeout(showImages, 500);
+});
+
 
 
  
