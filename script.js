e43227a78d867564e69695bddd9fc4a932bcd04a
jukka211 <158295066+jukka211@users.noreply.github.com>
2024-04-24 21:09:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -34,26 +34,26 @@ $(document).ready(function() {
 
 
 $(document).ready(function() {
-  // Check if the device is a desktop
-  if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
-    // Code to execute only on desktops
-    $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').click(function() {
+  $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').click(function() {
+    // Check if the device is a desktop
+    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+      var screenHeight = $(window).height(); // Get the height of the window
       var flexValue;
-      var windowHeight = $(window).height();
 
-      if (windowHeight >= 1500) {
-        flexValue = '58';
-      } else if (windowHeight >= 1200) {
-        flexValue = '25';
+      if (screenHeight > 1800) {
+        flexValue = Math.round(screenHeight / 2.5); // Use a different value for screens taller than 1800px
       } else {
-        flexValue = '10';
+        flexValue = Math.round(screenHeight / 1.59); // Default calculation for other screen heights
       }
 
-      $('#01, #02, #03, #04, #05, #06, #07, #08, #09, #10, #11, #12, #13, #14, #15, #16, #17, #18, #19, #20, #21, #22, #23').not(this).css('flex', '1');
       $(this).css('flex', flexValue);
-    });
-  }
+    }
+  });
 });
+
+
+
+
 
 
 
